41f1c66bfe62077e5c91e6bf2bb75b19512940ee
rykun <rykunek@hotmail.com>
2018-04-28 21:20:11 +0200

    Finish user signup

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,12 +8,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    assert_select "a[href=?]", signin_path
+    assert_select "a[href=?]", signup_path
 
     get contact_path
     assert_select "title", full_title("Contact")
 
-    get signin_path
+    get signup_path
     assert_select "title", full_title("Sign up")
   end
 end
